8233cd25acf5bbaa4f514664d3a2208b1ea13c28
Shahid Kamal <shahidkamal08@gmail.com>
2019-12-28 22:25:23 +0530

    with args no require

---
+++ src/lib.js
@@ -0,0 +1,35 @@
+let Worker, workerData;
+try {
+  ({ Worker, workerData } = require('worker_threads'));
+} catch (err) {
+  console.error(`worker_threads are not supported in your current node version ${process.version}.`);
+  process.exit(-1);
+}
+
+
+function fnWrapper(fn) {
+  const runStr = `
+    const { parentPort, workerData } = require('worker_threads');
+    const data = (${fn.toString()})(workerData);
+    parentPort.postMessage(data)
+  `;
+  return runStr;
+}
+
+function run(fn, workerData) {
+  if (!Worker) return;
+  return new Promise((resolve, reject) => {
+    const wrapped = fnWrapper(fn)
+    const fnStr = wrapped.toString();
+    // console.log(fnStr)
+    const worker = new Worker(fnStr, { eval: true, workerData });
+    worker.on('error', reject);
+    worker.on('message', resolve);
+    worker.on('exit', code => {
+      if (code != 0) reject(new Error('worker stopped with non-zero code.'));
+    })
+  })
+}
+
+
+module.exports = { run }
